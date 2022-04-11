8299d0e69eb1695231dc05707345812e282221ed
Adrien Fabre <adrien.fabre@dashlane.com>
2022-04-11 22:00:01 +0200

    First working version of sveltjs + p5js with data control component

---
+++ src/Controls.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    export let data;
+
+    const increaseValue = () => {
+        data.ruleNumber = data.ruleNumber+1;
+    }
+</script>
+
+<main>
+    <div>
+        <span>data : {JSON.stringify(data)}</span>
+        <button on:click={increaseValue}>Increase Value</button>
+    </div>
+</main>
---
+++ src/Simulation.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import P5 from 'p5';
+    export let data;
+
+    const sketch = (p5: P5) => {
+        let mover: Mover;
+        p5.setup = () => {
+            const canvas = p5.createCanvas(500, 500);
+            canvas.parent('canvasDiv');
+        };
+
+        // The sketch draw method
+        p5.draw = () => {
+            p5.background(30, 200, 30);
+            p5.text(data.ruleNumber, p5.width/2, p5.height/2);
+        };
+    };
+
+    new P5(sketch);
+</script>
+
+<main>
+    <div id="canvasDiv"></div>
+</main>
